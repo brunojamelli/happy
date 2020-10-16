592361704647b8cf73f366e2ed697aaf4cdf99a9
brunojamelli <brunojamelli@ufrn.edu.br>
2020-10-16 14:34:51 -0300

    using mapbox in react page

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { FiPlus } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
-
+import {Map, TileLayer} from 'react-leaflet';
 import mapMarkerImg from '../images/map-marker.svg';
+
 import '../styles/pages/orphanages-map.css';
+import 'leaflet/dist/leaflet.css';
 
 function OrphanagesMap() {
     return (
@@ -16,6 +18,13 @@ function OrphanagesMap() {
                     <p>Muitas crianças estão esperando a sua visita :)</p>
                 </header>
             </aside>
+            <Map
+                center={[-6.1524235, -36.1175205]}
+                zoom={15}
+                style={{ width: '100%', height: '100%' }}
+            >
+                <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} />
+            </Map>
             <Link to="" className="create-orphanage">
                 <FiPlus size={32} color="#FFF" />
             </Link>

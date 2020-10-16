081d70fb027a9c7c6bc8e7c9627de54503d4d794
brunojamelli <brunojamelli@ufrn.edu.br>
2020-10-16 20:56:15 -0300

    creation of post orphanage

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,32 @@
 import express from 'express';
 import './database/connection';
+import Orphanage from './models/Orphanage';
+import { getRepository } from 'typeorm';
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3333);
 
-app.post("/orphanages", (req, res) => { 
-    console.log("aaa");
-    res.status(200).send({
-        msg: "aijkaijajia"
-    })
+app.post("/orphanages",async (req, res) => {
+    const {
+        name,
+        latitude,
+        longitude,
+        about,
+        instructions,
+        opening_hours,
+        open_on_wekends,
+    } = req.body();
+
+    const orphRep = getRepository(Orphanage);
+
+    const orph = orphRep.create({
+        ...req.body
+    });
+
+    await orphRep.save(orph);
+
+    return res.status(201).json(orph);
 })

8b0c522556a99f7fd70a9ce7d2daac284ae469f1
brunojamelli <brunojamelli@ufrn.edu.br>
2020-10-16 11:22:01 -0300

    initing the landing page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,15 +1,28 @@
 import React from 'react';
-interface TitleData {
-  text: string;
-}
-
-
-function Title(props: TitleData) {
-  return <h1>{props.text}</h1>
-}
+import './styles/pages/landing.css';
+import './styles/global.css';
+import logoImg from './images/logo.svg';
 function App() {
   return (
-    <Title text="something"/>
+    <div id="page-landing">
+    <div className="content-wrapper">
+      <img src={logoImg} alt="Happy" />
+
+      <main>
+        <h1>Leve felicidade para o mundo</h1>
+        <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+      </main>
+
+      <div className="location">
+        <strong>Rio Grande do Norte</strong>
+        <span>Lajes</span>
+      </div>
+
+      {/* <Link to="/app" className="enter-app">
+        <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)" />
+      </Link> */}
+    </div>
+  </div>
   );
 }
 

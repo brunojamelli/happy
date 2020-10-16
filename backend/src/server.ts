e85b067222a0bcbd17f79196fec04bc6582e1147
brunojamelli <brunojamelli@ufrn.edu.br>
2020-10-16 16:42:46 -0300

    creation of database comunication with typeorm

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,13 @@
 import express from 'express';
+import './database/connection';
 
 const app = express();
 
 app.listen(3333);
+
+app.get("/users", (req, res) => { 
+    console.log("aaa");
+    res.status(200).send({
+        msg: "aijkaijajia"
+    })
+})

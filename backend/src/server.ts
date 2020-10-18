5cf7425a208d17c0ff3111f9415ca2ca6b97a7cb
brunojamelli <brunojamelli@ufrn.edu.br>
2020-10-17 21:43:07 -0300

    enable external access to API

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 import './database/connection';
 import routes from './routes';
@@ -6,6 +7,7 @@ import path from 'path';
 
 const app = express();
 
+app.use(cors);
 app.use(express.json());
 app.use(routes);
 app.use('/uploads',express.static(path.join(__dirname,'..', 'uploads')))

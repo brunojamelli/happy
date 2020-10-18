814d45c29b8cd15c0168ad55eeead6769fa6a703
brunojamelli <brunojamelli@ufrn.edu.br>
2020-10-18 14:30:54 -0300

    adding marker in map #frontweb

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { FiPlus } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
-import {Map, TileLayer} from 'react-leaflet';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 import mapMarkerImg from '../images/map-marker.svg';
+import Leaflet from 'leaflet';
 
 import '../styles/pages/orphanages-map.css';
 import 'leaflet/dist/leaflet.css';
-
+const mapIcon = Leaflet.icon({
+    iconUrl: mapMarkerImg,
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor:[170,2]
+})
 function OrphanagesMap() {
     return (
         <div id="page-map">
@@ -24,6 +30,15 @@ function OrphanagesMap() {
                 style={{ width: '100%', height: '100%' }}
             >
                 <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} />
+
+                <Marker
+                    icon={mapIcon}
+                    position={[-6.1524235, -36.1175205]}
+                >
+                    <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup">
+                        Orfanato
+                    </Popup>
+                </Marker>
             </Map>
             <Link to="" className="create-orphanage">
                 <FiPlus size={32} color="#FFF" />

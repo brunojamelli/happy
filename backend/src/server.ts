b0ef7e88988ca01adadfd36c825eb45bc83cf12e
brunojamelli <brunojamelli@ufrn.edu.br>
2020-10-18 15:16:08 -0300

    configuring the conexion of frontend web with the API

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -7,7 +7,7 @@ import path from 'path';
 
 const app = express();
 
-app.use(cors);
+// app.use(cors);
 app.use(express.json());
 app.use(routes);
 app.use('/uploads',express.static(path.join(__dirname,'..', 'uploads')))

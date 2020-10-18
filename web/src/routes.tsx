9d8c0668238e7d55174d6dafc2535bed1f61b95a
brunojamelli <brunojamelli@ufrn.edu.br>
2020-10-18 14:59:04 -0300

    ajusts in creation and showing orphanage pages

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Landing from './pages/Landing';
 import OrphanagesMap from './pages/OrphanagesMap';
@@ -9,13 +9,13 @@ import CreateOrphanage from './pages/CreateOrphanage';
 function Routes() {
   return (
     <BrowserRouter>
-      {/* <Switch> */}
+      <Switch>
         <Route path="/" exact component={Landing} />
         <Route path="/app" component={OrphanagesMap} />
         <Route path="/orphanages/create" component={CreateOrphanage} />
         <Route path="/orphanages/:id" component={Orphanages} />
 
-      {/* </Switch> */}
+      </Switch>
     </BrowserRouter>
   )
 }
